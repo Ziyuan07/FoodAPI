c5f257742ab24acde4f8dee62e6bea492933dfa8
Kykelvin1121 <yuanpang00@gmail.com>
2023-07-05 16:46:45 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -128,15 +128,6 @@ export default function Home() {
       )}
 
       <div className="mt-20 mb-10 text-center">
-        <p className="text-primary text-xs">
-          Made by RapidAPI DevRel Team -{" "}
-          <a
-            className="hover:text-active"
-            href="https://github.com/RapidAPI/DevRel-Examples-External"
-          >
-            See more examples like this
-          </a>
-        </p>
       </div>
     </div>
   );
